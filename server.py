47a7a522decbd35450f69efca87f9725e2b426c6
Juan Carlos Quirino <juanquirinoc@gmail.com>
2019-08-24 17:36:48 -0500

    Added endpoint

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 import os
 
-from flask import Flask, jsonify, make_response, request, render_template, Response
+from flask import Flask, jsonify, make_response, request, render_template, Response, app
 
 from flask_cors import CORS
 
-@app.route('/classifier', methods=['GET'])
+
+#Flask stuff
+app = Flask(__name__)
+CORS(app)
+app.config['MONGODB_SETTINGS'] = {'db':'gorditos', 'alias':'default'}
+
+@app.route('/classifier', methods=['POST'])
 def classifier():
-    return "OK"
+    return "HACKMTY"
